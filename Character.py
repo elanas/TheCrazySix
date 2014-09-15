8d2af37ff52653f7e2534d78af389317ca33b84d
Elana Stroud <estroud1@jhu.edu>
2014-09-15 18:43:34 -0400

    added Character abstract class

---
+++ Character.py
@@ -0,0 +1,19 @@
+import pygame
+
+Character extends sprite:
+
+    def __init__(self, w, h, x, y):
+        pass
+    
+    def changeDirection(self, n):
+        pass
+
+    def update(self):
+        pass
+
+    def move(self):
+        pass
+
+
+
+    
