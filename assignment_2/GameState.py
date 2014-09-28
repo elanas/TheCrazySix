92d8c8400ca3ce7a0b2184841a15d5d2ff61d437
Education <education@bumjin.home>
2014-09-28 17:08:34 -0400

    Updated

--- assignment_2/GameState.py
+++
@@ -1,9 +0,0 @@
-class GameState(object):
-    def __init__(self):
-        pass
-    def render(self):
-        pass
-    def update(self, time):
-        pass
-    def event(self, event):
-        pass
