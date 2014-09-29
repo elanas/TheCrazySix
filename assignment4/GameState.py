8cd1dc0a7184dca8b3a4af7e597f3a22cb59d5cf
Michael Stewart <mstewa34@jhu.edu>
2014-09-29 17:45:31 -0400

    initialized assignment4 folder with assignment3 files

---
+++ assignment4/GameState.py
@@ -0,0 +1,12 @@
+class GameState(object):
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
+
+    def update(self, time):
+        pass
+
+    def event(self, event):
+        pass
