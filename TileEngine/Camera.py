08c74c5b369997c57b5ea5f54029484f921b047a
Michael Stewart <mstewa34@jhu.edu>
2014-10-03 17:49:46 -0400

    Fixed issue with non (0, 0) container rect

--- TileEngine/Camera.py
+++ TileEngine/Camera.py
@@ -18,7 +18,7 @@ class Camera(object):
 			while curr_x - self.viewpoint.left< self.container.right:
 				curr_tile = self.tileEngine.get_tile_image(curr_x, curr_y)
 				tile_rect = curr_tile.get_rect()
-				area = pygame.Rect(curr_x - self.viewpoint.left, curr_y - self.viewpoint.top, tile_rect.width, tile_rect.height)
+				area = pygame.Rect((curr_x - self.viewpoint.left) + self.container.left, (curr_y - self.viewpoint.top) + self.container.top, tile_rect.width, tile_rect.height)
 				screen.blit(curr_tile, area)
 				curr_x += tile_rect.width
 			curr_y += tile_rect.height
