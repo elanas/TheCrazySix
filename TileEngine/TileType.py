8ebd1bf4a28fdb070ca263f5637b00562fe71b04
Michael Stewart <mstewa34@jhu.edu>
2014-10-08 13:08:18 -0400

    Added optional special attributes to type definition files and added the ability to have comments in tile definition files

--- TileEngine/TileType.py
+++ TileEngine/TileType.py
@@ -2,11 +2,13 @@ from asset_loader import AssetLoader
 
 
 class TileType(object):
+    STAIR_ATTR = "stair"
 
-    def __init__(self, loader, symbol, img_path, solid):
+    def __init__(self, loader, symbol, img_path, solid, special_attr):
         self.symbol = symbol
         self.solid = solid
         self.image = None
+        self.special_attr = special_attr
         self.load_image(loader, img_path)
 
     def load_image(self, loader, img_path):
