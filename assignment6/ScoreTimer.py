e450758ffcb5331b3ae5e28b675b38c9827c4d2e
shalarn1 <shalarn1@jhu.edu>
2014-10-19 17:31:17 -0400

    working on display

--- assignment6/ScoreTimer.py
+++ assignment6/ScoreTimer.py
@@ -11,9 +11,14 @@ class ScoreTimer():
         #Globals.SCREEN.fill(Globals.BACKGROUND_COLOR)
         font = pygame.font.SysFont("hannotatesc", 64)
         COLOR = pygame.Color("green")
-        health_surf = font.render("Time: ", True, COLOR)
+        health_string = Globals.PLAYER_HEALTH
+        if (Globals.PLAYER_HEALTH < 50):
+            COLOR = pygame.Color("yellow")
+        if (Globals.PLAYER_HEALTH < 25):
+            COLOR = pygame.Color("red")
+        health_surf = font.render("HP: " + str(health_string), True, COLOR)
         health_rect = health_surf.get_rect()
-        health_rect.topright = Globals.SCREEN.get_rect().topright
+        health_rect.topleft = Globals.SCREEN.get_rect().topleft
         health_rect.width = Globals.WIDTH/3
         #health_rect.top = Globals.SCREEN.get_rect().top + TITLE_PADDING
         Globals.SCREEN.blit(health_surf, health_rect)
