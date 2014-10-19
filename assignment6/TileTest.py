04d4ad23c87913b0632eb9315247b16b1ad5ea7c
shalarn1 <shalarn1@jhu.edu>
2014-10-19 17:35:11 -0400

    fd

--- assignment6/TileTest.py
+++ assignment6/TileTest.py
@@ -39,8 +39,8 @@ class TileTest(GameState):
         self.has_collided = False
         self.enemySprites = pygame.sprite.Group()
         self.playerSprites = pygame.sprite.Group()
-        self.health = HealthBar()
-        #self.score_timer = ScoreTimer()
+        #self.health = HealthBar()
+        self.score_timer = ScoreTimer()
         for x in range(TileTest.NUM_ENEMY):
             # fix the positions they are added in and everything else
             # should work
@@ -55,8 +55,8 @@ class TileTest(GameState):
         self.camera.render(Globals.SCREEN)
         self.enemySprites.draw(Globals.SCREEN)
         self.playerSprites.draw(Globals.SCREEN)
-        self.health.render(Globals)
-        #self.score_timer.render(Globals)
+        #self.health.render(Globals)
+        self.score_timer.render(Globals)
 
     def update(self, time):
         self.player.update(time, self.camera, self.enemySprites)
