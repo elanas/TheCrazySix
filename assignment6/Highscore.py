6d2ff4ac68ea3c83be4a10762e7ec750a2459f7e
Elana Stroud <estroud1@jhu.edu>
2014-10-20 18:24:33 -0400

    trying to center highscores view

--- assignment6/Highscore.py
+++ assignment6/Highscore.py
@@ -2,8 +2,6 @@ import pygame
 
 from GameState import GameState
 from Globals import Globals
-from Player import Player
-from BorderPlayer import BorderPlayer
 from HighscoreManager import HighscoreManager
 import Menu
 
@@ -14,31 +12,19 @@ class Highscore(GameState):
         GameState.__init__(self)
         self.color = pygame.color.Color("black")
         self.time = 0.0
-        self.playerSprites = pygame.sprite.Group()
-        self.playerSprites.add(BorderPlayer(
-            Globals.WIDTH, Globals.HEIGHT, 0, 0, Player.INDEX_DOWN))
-        self.playerSprites.add(BorderPlayer(
-            Globals.WIDTH, Globals.HEIGHT,
-            Globals.WIDTH, 0, Player.INDEX_LEFT))
-        self.playerSprites.add(BorderPlayer(
-            Globals.WIDTH, Globals.HEIGHT, 0,
-            Globals.HEIGHT, Player.INDEX_RIGHT))
-        self.playerSprites.add(BorderPlayer(
-            Globals.WIDTH, Globals.HEIGHT,
-            Globals.WIDTH, Globals.HEIGHT, Player.INDEX_UP))
 
     def render(self):
         Globals.SCREEN.fill(Globals.BACKGROUND_COLOR)
         font = pygame.font.SysFont("hannotatesc", 64)
-        # font.
-        TITLE_PADDING = 75
-        VERT_SPACING = 75
+
+        TITLE_PADDING = 100
+        VERT_SPACING = 90
 
         COLOR = (7, 147, 240)
 
         title_surf = font.render("Highscores", True, (0, 0, 0))
         title_rect = title_surf.get_rect()
-        title_rect.centerx = Globals.SCREEN.get_rect().centerx - 120
+        title_rect.centerx = Globals.SCREEN.get_rect().centerx
         title_rect.centery = Globals.SCREEN.get_rect().centery
         title_rect.top = Globals.SCREEN.get_rect().top + TITLE_PADDING
         Globals.SCREEN.blit(title_surf, title_rect)
@@ -61,16 +47,8 @@ class Highscore(GameState):
         title_rect.centery += VERT_SPACING
         Globals.SCREEN.blit(op4, title_rect)
 
-        self.playerSprites.draw(Globals.SCREEN)
-        for p in self.playerSprites:
-            p.onDraw()
-
-        pygame.display.flip()
-
     def update(self, time):
-        self.time += time
-        self.playerSprites.update(time)
-
+        pass
     def event(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             Globals.STATE = Menu.Menu()
