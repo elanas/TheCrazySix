a23da8fb990327931c109e7dfdebfc30b4c706ef
Bumjin Kim <bkim63@jhu.edu>
2014-10-26 16:04:54 -0400

    pep8 style

--- assignment7/ScoreTimer.py
+++ assignment7/ScoreTimer.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class ScoreTimer():
 
     def __init__(self):
@@ -12,13 +13,17 @@ class ScoreTimer():
         COLOR = pygame.Color("white")
         time_string = pygame.time.get_ticks() - self.offset
         Globals.REMAINING_TIME = self.total_time - time_string
-        time_surf = self.font.render(str(Globals.REMAINING_TIME)[:2], True, COLOR)
+        time_surf = self.font.render(
+            str(Globals.REMAINING_TIME)[:2],
+            True,
+            COLOR
+        )
         if Globals.REMAINING_TIME < self.total_time / 4:
             COLOR = pygame.Color("red")
         time_rect = time_surf.get_rect()
         time_rect.topright = Globals.SCREEN.get_rect().topright
         time_rect.width = Globals.WIDTH / 4
         Globals.SCREEN.blit(time_surf, time_rect)
- 
+
     def update(self):
         pass
