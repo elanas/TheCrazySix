21d367d13a3d2ac511d4c9a2f87405d48f5d7368
Elana Stroud <estroud1@jhu.edu>
2014-10-29 18:07:26 -0400

    added enemies to how to play

--- assignment8/HowToPlay.py
+++ assignment8/HowToPlay.py
@@ -9,6 +9,10 @@ TITLE = "~How To Play~"
 TITLE_COLOR = pygame.color.Color("white")
 TITLE_PADDING = 100
 BACKGROUND_IMG = pygame.image.load("images/background.png")
+KEYBOARD_IMG = pygame.image.load("images/keyboard.png");
+ENEMY_IMG = pygame.image.load("images/zombie_walking_down_profile.png")
+STAIR_IMG = pygame.image.load("images/tiles/stair_right.png")
+
 
 
 class HowToPlay(GameState):
@@ -41,24 +45,18 @@ class HowToPlay(GameState):
     def render(self):
         Globals.SCREEN.fill(Globals.BACKGROUND_COLOR)
         Globals.SCREEN.blit(BACKGROUND_IMG, [0, 0])
+
+        Globals.SCREEN.blit(KEYBOARD_IMG, [50, 300])
+        Globals.SCREEN.blit(ENEMY_IMG, [400, 307])
+        Globals.SCREEN.blit(STAIR_IMG, [750, 307])
+
+
         # title_rect.top = Globals.SCREEN.get_rect().top + TITLE_PADDING
         Globals.SCREEN.blit(self.title_surf, self.title_rect)
         Globals.SCREEN.blit(self.alt_surf, self.alt_rect)
 
     def update(self, time):
         self.time_delta += time
-        # if self.time_delta >= Title.TIME_DELTA:
-        #     self.time_delta = 0
-        #     self.size += self.delta
-        #     self.font = pygame.font.Font(None, self.size)
-        #     self.title_surf = self.font.render(GAME_TITLE, True, TITLE_COLOR)
-        #     self.title_rect = self.title_surf.get_rect()
-        #     self.title_rect.centerx = Globals.SCREEN.get_rect().centerx
-        #     self.title_rect.centery = Globals.SCREEN.get_rect().centery
-        #     if self.size >= Title.INIT_SIZE + self.MAX_DELTA or \
-        #     self.size <= Title.INIT_SIZE - self.MAX_DELTA:
-        #         self.delta *= -1
-        # self.playerSprites.update(time)
 
     def event(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
