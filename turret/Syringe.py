de2204d0f0f70aa64d581eeef958f1cee3be53e7
mlbvsnba <mstewa34@jhu.edu>
2014-10-29 13:46:10 -0400

    added movement and collision detection with solid objects

--- turret/Syringe.py
+++ turret/Syringe.py
@@ -5,27 +5,29 @@ from asset_loader import AssetLoader
 
 class Syringe (pygame.sprite.Sprite):
     NEGATIVE_MARGIN = 2
+    VELOCITY = 200
 
     def __init__(self, x, y, left, left_img_path, right_img_path):
         super(Syringe, self).__init__()
-        self.x = x
-        self.y = y
         self.left = left
+        self.velocity = Syringe.VELOCITY
+        if self.left:
+            self.velocity *= -1
         self.loader = AssetLoader("images")
         self.is_dead = False
         if left:
             self.image = self.loader.load_image_alpha(left_img_path)
         else:
             self.image = self.loader.load_image_alpha(right_img_path)
-        self.init_rect(self.image.get_rect())
+        self.init_rect(self.image.get_rect(), x, y)
 
-    def init_rect(self, rect):
+    def init_rect(self, rect, x, y):
         self.rect = rect
         if self.left:
-            self.rect.right = self.x + Syringe.NEGATIVE_MARGIN
+            self.rect.right = x + Syringe.NEGATIVE_MARGIN
         else:
-            self.rect.left = self.x - Syringe.NEGATIVE_MARGIN
-        self.rect.centery = self.y
+            self.rect.left = x - Syringe.NEGATIVE_MARGIN
+        self.rect.centery = y
 
     def move(self, x_delta, y_delta):
         self.x += x_delta
@@ -38,7 +40,13 @@ class Syringe (pygame.sprite.Sprite):
         self.is_dead = True
 
     def update(self, time, camera):
-        pass
+        self.rect.x += time * self.velocity
+        radius = max(self.rect.size) * 1.5
+        solid_tiles = camera.get_solid_tiles(self.rect.center, radius)
+        solid_rects = [pair.rect for pair in solid_tiles]
+        collide_rects = self.rect.collidelistall(solid_rects)
+        if len(collide_rects) > 0:
+            self.is_dead = True
 
 
 class NormalSyringe(Syringe):
