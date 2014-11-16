49dcfafa6f498598b884d5ea5da7d373490977fa
shalarn1 <shalarn1@jhu.edu>
2014-11-16 17:25:09 -0500

    fixed syntax

--- Levels/SyringeLevel.py
+++ Levels/SyringeLevel.py
@@ -17,14 +17,14 @@ class SyringeLevel(Level):
     def event(self, event):
     	super(SyringeLevel, self).event(event)
 		if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-			 radius = max(self.player.rect.size) * 2
-			 special_tiles = self.camera.get_special_tiles(
-             self.player.rect.center, radius)
-             self.check_lever_collision(special_tiles)
+			radius = max(self.player.rect.size) * 2
+			special_tiles = self.camera.get_special_tiles(
+            self.player.rect.center, radius)
+            self.check_lever_collision(special_tiles)
 
     def check_lever_collision(self, special_tiles):
         lever_rects = [pair.rect for pair in special_tiles
-        				  if TileType.LEVER_LEFT_ATTR in pair.tile.special_attr]
+        				if TileType.LEVER_LEFT_ATTR in pair.tile.special_attr]
         temp_rect = self.player.rect.inflate(
             -Player.LEVER_OFFSET, -Player.LEVER_OFFSET)
         num_lever_left = len(temp_rect.collidelistall(lever_rects))
