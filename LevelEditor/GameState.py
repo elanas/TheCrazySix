4efc2ab730a8def5ac1c88e713ec7957898b0596
Michael Stewart <mstewa34@jhu.edu>
2014-12-12 18:41:27 -0500

    fixed editor

---
+++ LevelEditor/GameState.py
@@ -0,0 +1,16 @@
+from EventHandler import EventHandler
+
+
+class GameState(EventHandler):
+
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
+
+    def update(self, time):
+        pass
+
+    def event(self, event):
+        pass
--- LevelEditor/LevelEditor.py
+++ LevelEditor/LevelEditor.py
@@ -12,7 +12,6 @@ from os.path import join
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from GameState import GameState
 
 
 class LevelEditor(GameState):
