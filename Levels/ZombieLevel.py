2d6d2777a6112248cc0760295f87991b6d8a1338
mlbvsnba <mstewa34@jhu.edu>
2014-12-13 17:05:18 -0500

    added music to zombie level

--- Levels/ZombieLevel.py
+++ Levels/ZombieLevel.py
@@ -5,7 +5,9 @@ from Level import Level
 class ZombieLevel(Level):
     DEF_NAME = 'map_def.txt'
     MAP_NAME = 'zombie_level.txt'
+    MUSIC_PATH = 'zombie_level.ogg'
 
     def __init__(self):
         super(ZombieLevel, self).__init__(
-            ZombieLevel.DEF_NAME, ZombieLevel.MAP_NAME)
+            ZombieLevel.DEF_NAME, ZombieLevel.MAP_NAME,
+            music_path=ZombieLevel.MUSIC_PATH)
