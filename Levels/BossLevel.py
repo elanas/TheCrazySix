24718c476a543eca75439e4faf7b671ba439d905
shalarn1 <shalarn1@jhu.edu>
2014-12-16 13:14:49 -0500

    VO

--- Levels/BossLevel.py
+++ Levels/BossLevel.py
@@ -9,13 +9,17 @@ class BossLevel(Level):
     MAP_NAME = 'boss.txt'
     SUBTITLE_TEXT = 'Find the antidote'
     SUBTITLE_LOOPS = 3
+    VOICE_OVER = 'transition4.ogg'
     MUSIC_PATH = 'syringe_level.ogg'
 
     def __init__(self, id=0):
         super(BossLevel, self).__init__(
-            BossLevel.DEF_NAME, BossLevel.MAP_NAME,
+            BossLevel.DEF_NAME, 
+            BossLevel.MAP_NAME,
+            init_music_path=BossFight.VOICE_OVER,
             music_path=BossLevel.MUSIC_PATH, id=id)
         self.show_subtitle(
             BossLevel.SUBTITLE_TEXT,
             BossLevel.SUBTITLE_LOOPS
         )
+
