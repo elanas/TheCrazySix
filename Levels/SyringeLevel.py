9d1b6f7b17924e1f753655f671db9096dbcd52bc
shalarn1 <shalarn1@jhu.edu>
2014-12-16 10:50:22 -0500

    adding voiceovers

--- Levels/SyringeLevel.py
+++ Levels/SyringeLevel.py
@@ -26,3 +26,40 @@ class SyringeLevel(Level):
             SyringeLevel.SUBTITLE_TEXT,
             SyringeLevel.SUBTITLE_LOOPS
         )
+    def start_music(self):
+        if self.channel or self.music_handle is None:
+            return
+        self.channel = self.music_handle.play(
+            loops=self.music_loops, fade_ms=Level.SOUND_FADE_TIME)
+        self.channel.set_endevent(Level.MUSIC_END_ID)
+        self.start_on_stop = True
+
+    def pause_music(self):
+        if self.channel:
+            self.channel.pause()
+            self.start_on_stop = True
+
+    def resume_music(self):
+        if self.channel:
+            self.channel.unpause()
+            self.start_on_stop = True
+
+    def stop_music(self):
+        if self.channel:
+            self.channel.fadeout(Level.SOUND_FADE_TIME)
+            self.channel = None
+            self.start_on_stop = False
+            self.music_handle = self.second_sound
+            self.music_loops = -1
+
+    def handle_raw_event(self, event):
+        if event.type == Level.MUSIC_END_ID and not self.switched_sound:
+            self.switched_sound = True
+            self.stop_subtitle()
+            self.music_handle = self.second_sound
+            self.music_loops = -1
+            if self.start_on_stop:
+                self.stop_music()
+                self.start_music()
+        else:
+            super(SyringeLevel, self).handle_raw_event(event)
