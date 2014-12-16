669acd9b42704c18726a8a5bd84887503f52e564
shalarn1 <shalarn1@jhu.edu>
2014-12-15 21:39:29 -0500

    adding music to maze level

--- Levels/PostZombieCutScene.py
+++ Levels/PostZombieCutScene.py
@@ -7,11 +7,12 @@ from Globals import Globals
 class PostZombieCutScene(Level):
     SUBTITLE_TEXT = 'You are disoriented'
     SUBTITLE_LOOPS = 5
+    MUSIC_PATH = 'maze.ogg'
 
     def __init__(self):
         super(PostZombieCutScene, self).__init__(
             ZombieCutScene.DEF_NAME,
-            ZombieCutScene.MAP_NAME,
+            ZombieCutScene.MAP_NAME, music_path=PostZombieCutScene.MUSIC_PATH,
             should_fade_in=False
         )
         self.show_subtitle(PostZombieCutScene.SUBTITLE_TEXT,
